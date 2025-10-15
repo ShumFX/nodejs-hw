1c9bb60e2e021f3071fae64a24a75bb008598ec1
ShumFX <alfadnepr@gmail.com>
2025-10-15 07:04:47 +0300

    fix1

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ import authRoutes from './routes/authRoutes.js';
 
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 
@@ -30,10 +30,6 @@ app.use(cookieParser());
 app.use(authRoutes);
 app.use(notesRoutes);
 
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
 
 app.use(notFoundHandler);
 
